31a6d29f0b0216527fa441617c8fbf4f8e71deec
xhander <xhanderdeveloper@gmail.com>
2024-12-01 21:09:15 -0500

    Actualizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoUsuarioComponent } from './pages/usuario/listado-usuario/listado-usuario.component';
+import { RegistroComponent } from './pages/usuario/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component : ListadoUsuarioComponent},
+  {path: "registro", component : RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/usuario/registro/registro.component.ts
+++ src/app/pages/usuario/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//Se importa el servicio de usuario
+import { UsuarioService, Usuario } from '../../../services/usuario.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,5 +11,34 @@ import { Component } from '@angular/core';
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  usuario:Usuario={
+    idUsuario: 0,
+    nombre: '',
+    apellido: '',
+    correoElectronico: '',
+    telefono: '',
+    password: '',
+    direccion: '',
+    fotoPerfil: '',
+    biografia: '',
+    sitioWeb: '',
+    activo: true,
+    ciudad: '',
+
+  }
+
+  constructor(private usurioservicio: UsuarioService){}
+
+  onSubmit():void{
+    this.usurioservicio.createUsuario(this.usuario).subscribe({
+      next:(result) => {
+        alert('Usuario creado con Exito!!!')
+      }, error:(e)=>{
+        console.log("El usuario no se creo por", e)
+        alert("Se produjo un error al crear el Usuario")
+      }
+
+    })
+  }
 
 }
