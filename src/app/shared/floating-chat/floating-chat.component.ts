da2b3d9538801aff9bc39480ba242ab9a5c08455
xhander <xhanderdeveloper@gmail.com>
2024-12-03 15:39:05 -0500

    creando chat de prueba

--- src/app/shared/floating-chat/floating-chat.component.ts
+++ src/app/shared/floating-chat/floating-chat.component.ts
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './floating-chat.component.css'
 })
 export class FloatingChatComponent {
-
   isMinimized: boolean = false;
   isVisible: boolean = true;
+  messages: string[] = []; // Lista de mensajes
+  newMessage: string = ''; // Mensaje nuevo
 
   toggleMinimize() {
     this.isMinimized = !this.isMinimized;
@@ -24,4 +25,22 @@ export class FloatingChatComponent {
     this.isVisible = true;
     this.isMinimized = false;
   }
+
+
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.messages.push(this.newMessage);
+      this.newMessage = '';
+      setTimeout(() => this.scrollToBottom(), 0);
+    }
+  }
+  
+  scrollToBottom() {
+    const messagesContainer = document.querySelector('.messages');
+    if (messagesContainer) {
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  }
+  
 }
